554f3c6ad5103a3efe02e02032f4283170e9ef18
Matthew Backman <matthewbackman@outlook.com>
2026-05-29 14:18:44 -0500

    #7 Added command to set angular velocity of holonomic drivetrains

---
+++ Driftless_Override_PROS/include/driftless/robot/commands/holonomic_drive_train/SetAngularVelocityCommand.hpp
@@ -0,0 +1,47 @@
+#ifndef __SET_ANGULAR_VELOCITY_COMMAND_HPP__
+#define __SET_ANGULAR_VELOCITY_COMMAND_HPP__
+
+#include "driftless/robot/commands/ICommand.hpp"
+/// @brief The namespace for driftless library code
+/// @author Matthew Backman
+namespace driftless {
+
+/// @brief The namespace for robot code
+/// @author Matthew Backman
+namespace robot {
+
+/// @brief The namespace for commands which can be sent to the robot
+/// @author Matthew Backman
+namespace commands {
+
+/// @brief Namespace containing commands for the holonomic drivetrain subsystem
+/// @author Matthew Backman
+namespace holonomic_drive_train {
+
+class SetAngularVelocityCommand : public ICommand {
+ private:
+  double m_angular_velocity;
+
+  bool m_is_normal;
+
+ public:
+  /// @brief Constructs a new SetAngularVelocityCommand with the given angular
+  /// velocity
+  /// @param angular_velocity __double__ The desired angular velocity of the
+  /// robot
+  /// @param is_normal __bool__ Whether the angular velocity is normalized
+  /// (between -1 and 1)
+  SetAngularVelocityCommand(double angular_velocity, bool is_normal = false);
+
+  subsystems::ESubsystem getTarget() const override;
+
+  bool isNormal() const;
+
+  double getAngularVelocity() const;
+};
+}  // namespace holonomic_drive_train
+}  // namespace commands
+}  // namespace robot
+}  // namespace driftless
+
+#endif
---
+++ Driftless_Override_PROS/src/driftless/robot/commands/holonomic_drive_train/SetAngularVelocityCommand.cpp
@@ -0,0 +1,17 @@
+#include "driftless/robot/commands/holonomic_drive_train/SetAngularVelocityCommand.hpp"
+
+namespace driftless::robot::commands::holonomic_drive_train {
+SetAngularVelocityCommand::SetAngularVelocityCommand(double angular_velocity,
+                                                     bool is_normal)
+    : m_angular_velocity(angular_velocity), m_is_normal(is_normal) {}
+
+subsystems::ESubsystem SetAngularVelocityCommand::getTarget() const {
+  return subsystems::ESubsystem::HOLONOMIC_DRIVE_TRAIN;
+}
+
+bool SetAngularVelocityCommand::isNormal() const { return m_is_normal; }
+
+double SetAngularVelocityCommand::getAngularVelocity() const {
+  return m_angular_velocity;
+}
+}  // namespace driftless::robot::commands::holonomic_drive_train
